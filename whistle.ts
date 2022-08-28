d6249659d44b65bd2cfc97c5383490ea4004f483
Nate <31741696+n-ate@users.noreply.github.com>
2022-08-28 00:14:12 -0700

    Delete whistle.ts

--- whistle.ts
+++
@@ -1,8 +0,0 @@
-import { RouteRegistry } from "./registries/Route.registry.js";
-
-export class Whistle {
-    public static Routing: RouteRegistry = new RouteRegistry();
-    constructor() {}
-
-    //public static CreateComponent(onchange: Function): PouchTemplate {
-}
